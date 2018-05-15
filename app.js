2f1a5a36e394cab9e2693942871ea474b5f6d8f8
Maksim Melnik <maksimusyan@gmail.com>
2018-05-16 01:25:27 +0300

    Разбор стурктуры Вопросов

--- app.js
+++ app.js
@@ -38,17 +38,55 @@ while (match = pattern.exec(clearStr)) {
 
 // Если массив xml-тестов не пустой
 if(xmls.length > 0){
+    // Проходим в цикле каждый Тест
     for(let index in xmls){
         // Преобразуем xml-строку в объект JS
         xml2js(xmls[index], function (err, result) {
-            console.log('======= '+result.Test.$.name+' =========');
+            // Атрибуты корневого элемента доступны через ключ $
+            let 
+                // ID в базе данных при сохранении Теста
+                testID,
+                // Название Теста
+                testName = result.Test.$.name,
+                // Код Теста
+                testCode = result.Test.$.code,
+                // Какой-то username Теста
+                testUsername = result.Test.$.username
+            ;
             
-            if(result.Test.Questions.length > 0){
+            // TODO: Тут сделать подключение к базе и сделать INSERT для Теста
+            // testID = 1;
+
+            // Если массив вопросов существует и он не пустой
+            if(typeof result.Test.Questions !== 'undefined' && result.Test.Questions.length > 0){
+                // Проходим в цикле каждый Вопрос
                 for(let iQ in result.Test.Questions){
+                    // question - это массив с одгним элементом, где:
+                    // question[0].$ - объект корневых атрибутов (значения, выбранные пользователем)
+                    // question[0].Answers - массив доступных вопросов
                     let question = result.Test.Questions[iQ].Question;
 
+                    // Предполагаю, что каких-то данных не существует. Проверяем:
+                    if (typeof question[0] === 'object' && typeof question[0].Answers === 'object'){
+                        let
+                            srcAnswer = question[0].$.answer.split('@#@'),
+                            // Значение выбранного ответа
+                            resultAnswerValue = srcAnswer[0],
+                            // ID выбранного ответа
+                            resultAnswerId = srcAnswer[1],
+                            // Тип вопроса
+                            questType = question[0].$.type,
+                            // Позиция вопроса
+                            questPosition = question[0].$.position,
+                            // Текст вопроса
+                            questText = question[0].$.text
+                        ;
 
-                    console.log(question[0].$.answer);
+
+                        //console.log(resultAnswerValue);
+                    }
+
+
 
                 }
             }
